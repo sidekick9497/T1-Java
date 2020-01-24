1a1665df2fd06412c35942fe620afd7ff648d2d8
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-01-24 15:35:02 +0530

    Add files via upload

---
+++ FormsInAngular/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { FormModelWayComponent } from './form-model-way/form-model-way.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormModelWayComponent
+  ],
+  imports: [
+    BrowserModule,
+    // make them available to all child components
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
