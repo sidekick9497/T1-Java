7b593726f79e0e8ca5020542f2bae326f58591ea
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-01-25 10:28:48 +0530

    Add files via upload

--- FormsInAngular/src/app/form-template-way/form-template-way.component.ts
+++ FormsInAngular/src/app/form-template-way/form-template-way.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-template-way',
@@ -19,16 +19,21 @@ export class FormTemplateWayComponent implements OnInit {
   // FormModule/ReactiveFormsModule : they inject a FormBuilder to constructors of all component
   constructor(formBuilder : FormBuilder) { 
 
-    this.myProductName =  new FormControl();
-
+    // this.myProductName =  new FormControl(<default value>, <validation rules>);
+    // this.myProductName =  new FormControl("", Validators.pattern(""));
+    // this.myProductName =  new FormControl("", Validators.maxLength(10));
+    this.myProductName =  new FormControl("", Validators.required);
     // formGroup is initialized using formbuilder
     this.myFormGroup = formBuilder.group({
         // members of a anonymous class
         // form controls
         "product_name" : this.myProductName,
-        "product_category" : new FormControl(),
-        "product_cost" : new FormControl()
-    });
+        // inbuilt angular validation rules
+        "product_category" : new FormControl("Books", Validators.required),
+        // "product_cost" : new FormControl("500", this.rangeCheck) // method name of constraint
+        // add multipe constriant
+        "product_cost" : new FormControl("500", Validators.compose([Validators.required, this.rangeCheck])) // array of validation rules 
+    }); 
 
       /*var first ={
         name : "First",
@@ -42,7 +47,25 @@ export class FormTemplateWayComponent implements OnInit {
     this.category = this.myFormGroup.controls['product_category'].value;
     this.cost = parseInt(this.myFormGroup.controls['product_cost'].value);
 
+
   }
+
+
+  // custom validation function
+  // 1. it will passed with a param : FormControl : Control on which constraint to be applied
+  // 2. in case of invalidity : return an object with a single data member
+  rangeCheck(txtControl : FormControl){
+    if(parseInt(txtControl.value) < 100 ||parseInt(txtControl.value) > 1000)
+        return {
+                  // <constraint name> : <boolean>
+                  "range" : true  // if constriant fails
+              }
+
+
+
+  }
+
+
 
   ngOnInit() {
   }
